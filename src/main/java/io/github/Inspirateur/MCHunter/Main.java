8b1bdfcb1d3d254377532c425267964d12855a95
Teo Orthlieb <inspirateur.pro@gmail.com>
2020-08-10 19:51:23 -0400

    Initial commit

---
+++ src/main/java/io/github/Inspirateur/MCHunter/Main.java
@@ -0,0 +1,15 @@
+package io.github.Inspirateur.MCHunter;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Plugin {
+	@Override
+	public void onDisable() {
+
+	}
+
+	@Override
+	public void onEnable() {
+
+	}
+}
